987e51491ac52f509343e74bcab49c849a55f961
Luphaestus <71532225+Luphaestus@users.noreply.github.com>
2026-01-14 14:28:02 +0000

    betteer py

--- src/iconFormating.py
+++ src/iconFormating.py
@@ -1,8 +1,16 @@
-dir = "/Users/lukan/Documents/ducc/DUCC-Website/public/images/icons/outline/"
+dir = "./public/images/icons/outline/"
 
 from os import listdir
 from os.path import isfile, join
+import shutil
 
+files = [f for f in listdir(dir) if isfile(join(dir, f))]
+
+for file in files:
+    if file.find("dp_") == -1: continue
+    newName = file[:file[:file.find("dp_")].rfind("_")] + file[file.rfind("."):]
+    shutil.move(dir+file, dir+newName)
+    
 files = [f for f in listdir(dir) if isfile(join(dir, f)) and f.endswith(".svg")]
 
 icons = ""
